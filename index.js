748e1c6554ff963f3209535f4088d582425d8678
swathiramesh20 <swathisrir@dckap.com>
2023-05-03 12:19:02 +0530

    updated

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
 let search = document.querySelector(".search")
 const input_value = document.querySelector("input")
-const display = document.querySelector(".display")
 const yourlocation = document.querySelector(".location")
+let icon = document.querySelector(".icon")
+let temperature = document.querySelector(".temperature")
+let weather_temperature = document.querySelector(".weather_temperature")
+let weather_status = document.querySelector(".weather_status")
+
+
+
 
 const alert = document.querySelector(".alert")
 
 yourlocation.addEventListener("click", (e) => {
-    console.log(e.target)
     navigator.geolocation.getCurrentPosition((location) => {
         let latitude = location.coords.latitude
         let longitude = location.coords.longitude
         fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=2c6823df05f8bb675e7f4e01e7870081`)
             .then(res => res.json())
             .then(json => {
-                console.log(json)
-                generatingdata(json)
+                creatingdata(json)
             })
             .catch((error) => console.log(error))
 
@@ -38,7 +42,7 @@ search.addEventListener("click", () => {
 
             }
             else {
-                generatingdata(json)
+                creatingdata(json)
 
             }
 
@@ -50,24 +54,15 @@ search.addEventListener("click", () => {
 
 
 
-function generatingdata(json) {
-    let icon = document.createElement("img")
+function creatingdata(json) {
     icon.setAttribute("Src",`https://api.openweathermap.org/img/w/${json.weather[0].icon}.png`)
-    let temperature = document.createElement("h2")
     temperature.innerText = json.name
-    // console.log(temperature);
 
-    let weather_temperature = document.createElement("h3")
     weather_temperature.innerHTML = `${Math.floor(json.main.temp - 273)}<span>${"&#176"}</span><span>${"C"}</span>`;
     // console.log(weather_temperature);
 
-    let weather_status = document.createElement("p")
     weather_status.innerText = json.weather[0].main
     // console.log(weather_status);
-    display.appendChild(icon)
-
-    display.appendChild(temperature)
-    display.appendChild(weather_temperature)
-    display.appendChild(weather_status)
+ 
 
 }
